eb4c9a7cad30fd82d225b6e50a2eaa368d259f8b
vlstr <vlungenstrass@live.de>
2019-02-16 17:56:19 +0100

    Init Spring Boot project

---
+++ bcm-backend/src/main/java/com/vlstr/bcmbackend/BcmBackendApplication.java
@@ -0,0 +1,14 @@
+package com.vlstr.bcmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcmBackendApplication.class, args);
+	}
+
+}
+
---
+++ bcm-backend/src/test/java/com/vlstr/bcmbackend/BcmBackendApplicationTests.java
@@ -0,0 +1,17 @@
+package com.vlstr.bcmbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BcmBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
